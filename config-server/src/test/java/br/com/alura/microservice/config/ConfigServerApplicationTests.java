caa8eb3491967c202a448056a58d081bfda0b7a6
Jonathan Crespo <sicrano3000@gmail.com>
2022-04-13 09:24:12 -0300

    alterando versão do java

--- config-server/src/test/java/br/com/alura/microservice/config/ConfigServerApplicationTests.java
+++ config-server/src/test/java/br/com/alura/microservice/config/ConfigServerApplicationTests.java
@@ -1,6 +1,6 @@
 package br.com.alura.microservice.config;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
