e03b3a41480fb740a7c6e9bd6038382fed6c113a
Jonathan Crespo <sicrano3000@gmail.com>
2022-04-16 16:46:41 -0300

    visualizando client side do load Balance em ação

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,10 +1,12 @@
 package br.com.alura.microservice.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
 
 import br.com.alura.microservice.loja.dto.CompraDTO;
 import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
@@ -14,12 +16,20 @@ public class CompraService {
 	
 	@Autowired
 	private RestTemplate client;
+	
+	@Autowired
+	private DiscoveryClient eurekaClient;
 
 	public void realizaCompra(CompraDTO compra) {
 		
 		ResponseEntity<InfoFornecedorDTO> exchange = 
 			client.exchange("http://fornecedor/info/"+compra.getEndereco().getEstado(),
 			HttpMethod.GET, null, InfoFornecedorDTO.class);
+		
+		eurekaClient.getInstances("fornecedor").stream()
+		.forEach(fornecedor -> {
+			System.out.println("localhost:"+fornecedor.getPort());
+		});
 		
 		System.out.println(exchange.getBody().getEndereco());
 		
